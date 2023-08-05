fe152b2b92f8ffc8ff192db90205d5805230e73b
Sergey Malinin <smalinin@openlinksw.com>
2023-08-06 01:22:10 +0300

    Update demo

--- js/chat.js
+++ js/chat.js
@@ -1,6 +1,35 @@
 class ChatUI {
   constructor({view}) {
     this.view = view;
+    this.md = markdownit({
+      html:         true,
+      xhtmlOut:     true,
+      breaks:       true,
+      langPrefix:   'language-',  // CSS language prefix for fenced blocks. Can be
+                              // useful for external highlighters.
+      linkify:      true,        // Autoconvert URL-like text to links
+
+      typographer:  false,
+
+  // Double + single quotes replacement pairs, when typographer enabled,
+  // and smartquotes on. Could be either a String or an Array.
+  //
+  // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
+  // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
+      quotes: '“”‘’',
+
+      highlight: function (str, lang) {
+         if (lang && hljs.getLanguage(lang)) {
+           try {
+             return '<pre class="hljs"><code>' +
+                     hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
+                   '</code></pre>';
+           } catch (__) {}
+         }
+
+         return '<pre class="hljs"><code>' + self.md.utils.escapeHtml(str) + '</code></pre>';
+       }
+     });
   }
 
   timeSince(date) 
@@ -112,7 +141,8 @@ class ChatUI {
 
       html.push(
         `<div class="block block-strong medium-inset" id="item_${id}">`
-       +`<p>${v.text}`
+       + this.md.render(v.text)
+//??--       +`<p>${v.text}`
        +`</div>`);
 
       id++; 
